7e994b19addc61a1d0d044f2339caf11e8099b66
kimdaun713 <59947577+kimdaun713@users.noreply.github.com>
2022-04-05 20:08:26 +0900

    Create UserManager.sol
    userManager

---
+++ Smart_Contract/UserManager.sol
@@ -0,0 +1,43 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract UserManager {
+  
+  struct User{
+      string passsword;
+      bool isUser;
+      uint256 lastLogin;
+  }
+
+  mapping(string => User) user;
+  function register(
+      string memory _id,
+      string memory _password
+  ) public returns (bool) {
+      if(user[_id].isUser == false){
+      user[_id].passsword = _password;
+      user[_id].isUser = true; 
+      return true; // 성공
+      }
+      return false; //실패 
+  }
+
+  function loginUser(string memory _id, string memory _password, uint _lastDate) public returns (bool) {
+       if(keccak256(abi.encodePacked(user[_id].passsword)) == keccak256(abi.encodePacked(_password))){
+           user[_id].lastLogin = _lastDate;
+            return true; 
+       }
+       else{
+           return false;
+       }
+  }
+
+  function deleteUser(string memory _id) public view returns (bool) {
+      if(user[_id].isUser == true){
+        user[_id].isUser == false;
+        return true;
+      }
+      return false;
+
+  }
+
+}
