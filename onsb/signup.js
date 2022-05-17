a8bfde2349d5e1ba2c7f81c0affa4f9e1080e298
kimdaun713 <59947577+kimdaun713@users.noreply.github.com>
2022-05-17 14:58:25 +0900

    Update signup.js

--- onsb/signup.js
+++ onsb/signup.js
@@ -97,20 +97,20 @@ async function signup(){
             }
         ]
         // 프라이빗 네트워크 연결
-        var web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.230.154:8545"));
+        var web3 = new Web3(new Web3.providers.HttpProvider("http://000.000.000.000:0000"));
          // 연결 계좌 확인
-         var bal = await web3.eth.getBalance("0x1de4635765c03a76dccf125d60aee010e3ff02c1");
+         var bal = await web3.eth.getBalance("0x552d12E774691CE7cA7aac165d40aaC6f60aca6E");
          console.log(bal);
          //web3.eth.defaultAccount = web3.eth.accounts[0];
       // 계정 unlock
-         web3.eth.personal.unlockAccount("0x1de4635765c03a76dccf125d60aee010e3ff02c1", "1234")
+         web3.eth.personal.unlockAccount("0x552d12E774691CE7cA7aac165d40aaC6f60aca6E", "gkdid112")
          .then(console.log('Account unlocked!'));
       // contract 연결
-         var contract = new web3.eth.Contract(ABI,"0x0a50Bce67189cFBA6a9eE924F8aDa42A8703298D");
+         var contract = new web3.eth.Contract(ABI,"0x275159C9C60A538667140Ef31CC19AAeB3809345");
          console.log(contract);
-         //var result = await contract.methods.checkUser(id).call({from:'0x1de4635765c03a76dccf125d60aee010e3ff02c1'});
+         //var result = await contract.methods.checkUser(id).call({from:'00x552d12E774691CE7cA7aac165d40aaC6f60aca6E'});
       // 회원 가입
-         var result = await contract.methods.register(id,psd).send({from:'0x1de4635765c03a76dccf125d60aee010e3ff02c1'})
+         var result = await contract.methods.register(id,psd).send({from:'0x552d12E774691CE7cA7aac165d40aaC6f60aca6E'})
          .then(function(receipt){});
          console.log(result);
     }catch(error){
