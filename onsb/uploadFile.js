9bf1b8c291844bb56bfc1301285170b638f5f291
Hyunjoo-5584824 <64509600+Hyunjoo-5584824@users.noreply.github.com>
2022-06-06 02:47:19 +0900

    Update uploadFile.js
    set fileList and link ipfs

--- onsb/uploadFile.js
+++ onsb/uploadFile.js
@@ -4,28 +4,141 @@
 
 
 let formData = new FormData();
+let ipfsLink="";
+const ABI = [
+                {
+                        "inputs": [
+                                {
+                                        "internalType": "string",
+                                        "name": "_key",
+                                        "type": "string"
+                                },
+                                {
+                                        "internalType": "uint256",
+                                        "name": "fileIdx",
+                                        "type": "uint256"
+                                }
+                        ],
+                        "name": "deleteFile",
+                        "outputs": [],
+                        "stateMutability": "nonpayable",
+                        "type": "function"
+                },
+                {
+                        "inputs": [
+                                {
+                                        "internalType": "string",
+                                        "name": "_key",
+                                        "type": "string"
+                                },
+                                {
+                                        "internalType": "string",
+                                        "name": "_fileName",
+                                        "type": "string"
+                                },
+                                {
+                                        "internalType": "string",
+                                        "name": "_fileHash",
+                                        "type": "string"
+                                }
+                        ],
+                        "name": "set",
+                        "outputs": [],
+                        "stateMutability": "nonpayable",
+                        "type": "function"
+                },
+                {
+                        "inputs": [
+                                {
+                                        "internalType": "string",
+                                        "name": "_key",
+                                        "type": "string"
+                                }
+                        ],
+                        "name": "getFileInfo",
+                        "outputs": [
+                                {
+                                        "components": [
+                                                {
+                                                        "internalType": "string[]",
+                                                        "name": "fileName",
+                                                        "type": "string[]"
+                                                    },
+                                                {
+                                                        "internalType": "string[]",
+                                                        "name": "list",
+                                                        "type": "string[]"
+                                                }
+                                        ],
+                                        "internalType": "struct fileShare.fileList",
+                                        "name": "",
+                                        "type": "tuple"
+                                }
+                        ],
+                        "stateMutability": "view",
+                        "type": "function"
+                },
+                {
+                        "inputs": [
+                                {
+                                        "internalType": "string",
+                                        "name": "_key",
+                                        "type": "string"
+                                }
+                        ],
+                        "name": "size",
+                        "outputs": [
+                                {
+                                        "internalType": "uint256",
+                                        "name": "",
+                                        "type": "uint256"
+                                }
+                        ],
+                        "stateMutability": "view",
+                        "type": "function"
+                }
+        ]
 
-function onFileUpload(){
-    let fileInput = document.getElementById("files");
-    const selectedFile = [...fileInput.files];
-    console.log(selectedFile);
-    for(let i = 0; i < selectedFile.length; i++){
-        addList(selectedFile[i].name);
-    }
-    
+
+async function uploadToNet(id, name, link){
+        var web3 = new Web3(new Web3.providers.HttpProvider("http://000.000.000.000:30000"));
+        await web3.eth.personal.unlockAccount("0x552d12E774691CE7cA7aac165d40aaC6f60aca6E", "gkdid112", 900000000);
+        var contract = new web3.eth.Contract(ABI, "0xADB4f75b02e171e3300Fb7E3279ed401FE2A6AC1");
+        var result = await contract.methods.set(id, name, link).send({from:"0x552d12E774691CE7cA7aac165d40aaC6f60aca6E"});
+        var check = await contract.methods.getFileInfo(id).call({from:"0x552d12E774691CE7cA7aac165d40aaC6f60aca6E"});
+        console.log(check);
 }
 
-function addList(text){
+async function anyLoadFile(){
+         var web3 = new Web3(new Web3.providers.HttpProvider("http://000.000.000.000:30000"));
+        await web3.eth.personal.unlockAccount("0x552d12E774691CE7cA7aac165d40aaC6f60aca6E", "gkdid112", 900000000);
+        var contract = new web3.eth.Contract(ABI, "0xADB4f75b02e171e3300Fb7E3279ed401FE2A6AC1");
+        var check = await contract.methods.getFileInfo(id).call({from:"0x552d12E774691CE7cA7aac165d40aaC6f60aca6E"});
+        console.log(check);
+        //console.log(check.list[0]);
+        for(let i = 0; i< check.fileName.length; i++){
+                addList(check.fileName[i],check.list[i]);
+
+        }
+
+}
+
+function addList(text, link){
     const li = document.createElement("dt");
     li.setAttribute('id', text);
 
     const textNode = document.createTextNode(text);
     const btn = document.createElement('button');
     const btntext = document.createTextNode('open');
-    btn.appendChild(btntext);
+        btn.id = "openbtn"+link;
+        console.log(btn.id);
+        btn.appendChild(btntext);
     li.appendChild(textNode);
     li.appendChild(btn);
+$(document).on("click", "#openbtn"+link, function(){
+                console.log($(this));
+                window.open("https://ipfs.infura.io/ipfs/"+ link);
+        });
 
     document.getElementById('file-lists').appendChild(li);
-
 }
